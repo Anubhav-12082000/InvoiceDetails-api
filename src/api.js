527eecf71c50793405bed367d507c97afb96210a
Anubhav <anubhavpanfer@gmail.com>
2021-12-21 21:05:24 +0530

    second commit

--- src/api.js
+++ src/api.js
@@ -1,20 +1,17 @@
-import express, { Router } from 'express';
-import serverless from 'serverless-http';
+import express, { Router } from "express";
+import serverless from "serverless-http";
 
 const app = express();
 const router = Router();
 
-
-router.get('/', (req, res) => {
-    const value = req.query.value
-    res.json({
-        'hello': 'hi',
-        "value": value
-    });
+router.get("/", (req, res) => {
+  const value = req.query.value;
+  res.json({
+    hello: "hi",
+    value: value,
+  });
 });
 
-
-app.use('/api', router);
-
+app.use("/.netlify/functions/api", router);
 
 export const handler = serverless(app);
